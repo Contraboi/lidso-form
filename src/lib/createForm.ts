7807c682d35f6fe8c87abbd193adb6759aaf699a
Selmir Nedžibi <69647487+Contraboi@users.noreply.github.com>
2023-06-15 00:23:04 +0200

    Add onInput validation add formState remove onblur

--- src/lib/createForm.ts
+++ src/lib/createForm.ts
@@ -1,4 +1,3 @@
-import { createSignal } from "solid-js";
 import { createStore, reconcile } from "solid-js/store";
 
 type LidsoFormElement =
@@ -12,7 +11,16 @@ type Errors<T> = Partial<Record<keyof T, string>>;
 type Refs<T> = Partial<Record<keyof T, LidsoFormElement>>;
 type CreateFormOptions<T> = {
   onSubmit: (values: T) => Promise<void> | void;
-  initialValues?: T;
+  initialValues: T;
+};
+type FormState<T> = {
+  isDirty: boolean;
+  dirtyFields: Array<keyof T>;
+  touchedFields: Array<keyof T>;
+  submitCount: number;
+  isValid: boolean;
+  isLoading: boolean;
+  errors: Errors<T>;
 };
 
 /**
@@ -45,6 +53,13 @@ type CreateFormOptions<T> = {
  *   </form>
  * );
  */
+
+type PartialOrType<T, K> = T extends undefined
+  ? Partial<K>
+  : T extends K
+  ? K
+  : Partial<K>;
+
 export const createForm = <Values extends object>(
   options: CreateFormOptions<Values>
 ) => {
@@ -53,68 +68,106 @@ export const createForm = <Values extends object>(
   let refs: Refs<Values> = {};
   let _validators: Validators<Values> = {};
 
-  const [isLoading, setIsLoading] = createSignal(false);
-  const [errors, setErrors] = createStore<Errors<Values>>();
-  const [fields, setFields] = createStore<Values>(
-    options?.initialValues ? options.initialValues : ({} as Values)
+  const [fields, setFields] = createStore<Values | Partial<Values>>(
+    options.initialValues ?? {}
   );
 
-  const _setAdequateValue = (ref: Refs<Values>[Key]) => {
-    const key = ref!.name as any;
-    if (!key) return;
+  const [formState, setFormState] = createStore<FormState<Values>>({
+    isDirty: false,
+    dirtyFields: [],
+    touchedFields: [],
+    submitCount: 0,
+    isLoading: false,
+    isValid: false,
+    errors: {},
+  });
+
+  const _setAdequateValue = (key: Key, ref: Refs<Values>[Key]) => {
+    setFormState("isDirty", true);
+
+    setFormState("touchedFields", (prev) => {
+      if (!prev.includes(key)) prev.push(key);
+      return prev;
+    });
 
     if (ref?.type === "number") {
+      // @ts-ignore
       setFields(key, parseInt(ref.value));
     } else if (ref?.type === "checkbox") {
+      // @ts-ignore
       setFields(key, (ref as HTMLInputElement).checked);
     } else {
+      // @ts-ignore
       setFields(key, ref!.value);
     }
   };
 
-  const _validate = () => {
+  const _setInitialValues = (ref: LidsoFormElement, key: Key) => {
+    if (options.initialValues) {
+      if (ref?.type === "number") {
+        ref.value = String(options.initialValues[key]);
+      } else if (ref?.type === "checkbox") {
+        (ref as HTMLInputElement).checked = Boolean(options.initialValues[key]);
+      } else {
+        ref.value = String(options.initialValues[key]);
+      }
+    }
+  };
+
+  const _validate = (shouldFocus: boolean) => {
     const errors: Errors<Values> = {};
     let firstErrorKey: Key | undefined;
+
     for (const key in _validators) {
       const validator = _validators[key];
 
       if (typeof validator === "string") {
         if (!firstErrorKey) firstErrorKey = key;
         errors[key] = validator;
-        refs[firstErrorKey]?.focus();
+
+        if (shouldFocus) refs[firstErrorKey]?.focus();
       }
     }
 
-    setErrors(reconcile(errors));
-    return Object.keys(errors).length === 0;
+    if (formState.submitCount > 0) setFormState("errors", reconcile(errors));
+
+    const isFormValid = Object.keys(errors).length === 0;
+    setFormState("isValid", isFormValid);
+    return isFormValid;
   };
 
   const register = (key: Key, validators?: Array<boolean | string>) => ({
-    ref: (ref: HTMLInputElement | HTMLTextAreaElement) => {
+    ref: (ref: LidsoFormElement) => {
       const instance = (refs[key] = ref);
+
+      _setInitialValues(instance, key);
+
       if (validators) {
         for (const validator of validators) {
           _validators[key] = validator;
         }
       }
 
-      instance.oninput = () => _setAdequateValue(instance);
-      instance.onblur = () => !errors[key] && _validate();
+      instance.addEventListener("input", () => {
+        _setAdequateValue(key, instance);
+        _validate(false);
+      });
     },
     name: key,
   });
 
   const submitForm = async (e: Event) => {
     e.preventDefault();
+    setFormState("submitCount", (prev) => prev + 1);
 
-    if (_validate()) {
-      setIsLoading(true);
+    if (_validate(true)) {
+      setFormState("isLoading", true);
       try {
         await options.onSubmit(fields as Values);
       } catch (error: any) {
         console.error(error);
       } finally {
-        setIsLoading(false);
+        setFormState("isLoading", false);
       }
     }
   };
@@ -122,9 +175,8 @@ export const createForm = <Values extends object>(
   return {
     fields,
     setFields,
-    isLoading,
     submitForm,
     register,
-    errors,
+    formState,
   };
 };
